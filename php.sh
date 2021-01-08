5e567b571e9f60ec3dd6f9ca7e9f552096b4caed
Heitor de Souza <heisou06@gmail.com>
2021-01-08 19:17:44 -0400

    Ajustes na configuraçao

---
+++ php.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+NAME_CONTAINER="$(docker ps --filter "label=command.php" --filter status=running --format "{{.Names}}")"
+#docker exec -it --user $(id -u):$(id -g) projeto-laravel php "$@"
+echo "Executado PHP no container $NAME_CONTAINER"
+echo "WorkDir Container: /var/www/html/"
+echo "WorkDir Local: $(pwd)/xastre-market-app/"
+docker exec -it --user $(id -u):$(id -g) $NAME_CONTAINER php "$@"
+
+exit $?
