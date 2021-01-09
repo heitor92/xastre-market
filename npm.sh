87f7a693f43f63125a0089ac05972cf774787358
Heitor de Souza <heisou06@gmail.com>
2021-01-09 03:21:09 -0400

    Ajustes

--- npm.sh
+++ npm.sh
@@ -2,9 +2,9 @@
 
 NAME_CONTAINER="$(docker ps --filter "label=command.php" --filter status=running --format "{{.Names}}")"
 #docker exec -it --user $(id -u):$(id -g) projeto-laravel composer "$@"
-echo "Executado composer no container $NAME_CONTAINER"
+echo "Executado npm no container $NAME_CONTAINER"
 echo "WorkDir Container: /var/www/html/"
 echo "WorkDir Local: $(pwd)/xastre-market-app/"
-docker exec -it --user $(id -u):$(id -g) $NAME_CONTAINER composer "$@"
+docker exec -it --user $(id -u):$(id -g) $NAME_CONTAINER npm --prefix=$HOME/.npm "$@"
 
 exit $?
