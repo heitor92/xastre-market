cd7d282249cf4095c0650c1b25dc225c6f5c03c0
heitor92 <heitor.souza21@gmail.com>
2021-10-06 21:04:18 +0000

    correção de URL insegura http

--- xastre-market-app/resources/views/products.blade.php
+++ xastre-market-app/resources/views/products.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <link href="/css/app.css" rel="stylesheet">
     <title>Xastre Market</title>
     <script src="/js/app.js"></script>
